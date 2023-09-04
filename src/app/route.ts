8de8aec4803fa69e83855318e446af85ef791f97
Amon Sawamura <amotarao@gmail.com>
2023-09-04 16:09:25 +0900

    force-dynamic

--- src/app/route.ts
+++ src/app/route.ts
@@ -3,3 +3,5 @@ import { NextResponse } from "next/server";
 export async function GET():Promise<NextResponse> {
   return NextResponse.json({ now: Date.now() });
 }
+
+export const dynamic = 'force-dynamic';
